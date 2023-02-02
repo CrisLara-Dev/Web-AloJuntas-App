b85acdf05f1b1f63151e7a0096fc010be0396c91
CristianLara-blip <82177927+CristianLara-blip@users.noreply.github.com>
2023-02-02 18:05:38 -0500

    update

---
+++ src/js/pages/modal.js
@@ -0,0 +1,13 @@
+import slickCarousel from 'slick-carousel';
+const d = document, c = console.log;
+
+export class Modal {
+    
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        
+    }
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,12 @@
 import $ from 'jquery';
 window.$ = $;
-import { Nosotros } from './pages/nosotros.js';
-import { Nav } from './components/nav.js';
+import { Modal } from './pages/modal.js';
 import { Header2 } from './pages/header2.js';
 import { Homefreemium } from './pages/home-freemium.js';
 import { Guardadospremium } from './pages/guardados-premium.js';
 
 document.addEventListener('DOMContentLoaded',() => {
-	new Nosotros();
+	new Modal();
 	new Header2();
 	new Homefreemium();
 	new Guardadospremium();
