73ec9db6a03d2b22808a42c3881addd71317883d
CristianLara-blip <82177927+CristianLara-blip@users.noreply.github.com>
2023-02-04 04:41:22 -0500

    update

--- src/js/pages/home-freemium.js
+++ src/js/pages/home-freemium.js
@@ -5,6 +5,7 @@ export class Homefreemium {
     
     constructor(){
         this.init();
+        this.init2();
     }
 
     init(){
@@ -52,4 +53,49 @@ export class Homefreemium {
             });
         });
     }
+    init2(){
+      window.addEventListener('load', function(){
+          new Glider(document.querySelector('.lista3'),{
+              slidesToShow:1,
+              slidesToScroll:1,
+              draggable: true,
+              arrows: {
+                  prev: '.anterior3',
+                  next: '.siguiente3'
+              },
+              responsive: [
+                  {
+                    // screens greater than >= 775px
+                    breakpoint: 768,
+                    settings: {
+                      // Set to `auto` and provide item width to adjust to viewport
+                      slidesToShow: '2',
+                      slidesToScroll: '2',
+                      itemWidth: 150,
+                      duration: 0.25
+                    }
+                  },{
+                  // screens greater than >= 775px
+                  breakpoint: 992,
+                  settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: '3',
+                    slidesToScroll: '3',
+                    itemWidth: 150,
+                    duration: 0.25
+                  }
+                  },{
+                    // screens greater than >= 1024px
+                    breakpoint: 1236,
+                    settings: {
+                      slidesToShow: 4,
+                      slidesToScroll: 4,
+                      itemWidth: 150,
+                      duration: 0.25
+                    }
+                  }
+                ]
+          });
+      });
+  }
 }
